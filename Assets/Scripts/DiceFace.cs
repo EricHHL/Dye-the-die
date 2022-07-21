ed3567ffc8367416e2f90d4aac2cd6653456dc2e
Eric Lopes <erichhlopes@gmail.com>
2022-07-21 09:58:37 -0400

    dice faces control

---
+++ Assets/Scripts/DiceFace.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class DiceFace {
+    public Vector3 direction;
+    public Transform transform;
+    public string name;
+    public int value = 0;
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,8 +8,20 @@ public class Player : MonoBehaviour {
     private bool isMoving = false;
     public Grid grid;
 
-    void Start() {
+    public AnimationCurve tryRollAnimCurve;
 
+    DiceFace[] faces = new DiceFace[]{
+        new DiceFace{direction = Vector3.forward, name = "forward"},
+        new DiceFace{direction = Vector3.right, name = "right"},
+        new DiceFace{direction = Vector3.back, name = "back"},
+        new DiceFace{direction = Vector3.left, name = "left"},
+        new DiceFace{direction = Vector3.up, name = "up"},
+        new DiceFace{direction = Vector3.down, name = "down"}
+    };
+
+
+    void Start() {
+        initializeFaces();
     }
 
     void Update() {
@@ -29,10 +41,22 @@ public class Player : MonoBehaviour {
 
     bool roll(Vector3 direction) {
         if (isMoving) return false;
+
         isMoving = true;
         var anchor = transform.position + (direction + Vector3.down) * 0.5f;
         var axis = Vector3.Cross(Vector3.up, direction);
         float rot = 0f;
+
+        if (!canRollTo(direction)) {
+            DOTween.To(() => rot, newRot => {
+                transform.RotateAround(anchor, axis, newRot - rot);
+                rot = newRot;
+            }, 12, 0.3f).SetEase(tryRollAnimCurve).OnComplete(() => {
+                isMoving = false;
+            });
+            return false;
+        }
+
         DOTween.To(() => rot, newRot => {
             transform.RotateAround(anchor, axis, newRot - rot);
             rot = newRot;
@@ -44,12 +68,48 @@ public class Player : MonoBehaviour {
     }
 
     void onMovementEnd() {
-        var tile = grid.getTile(transform.position);
+        Tile tile = grid.getTile(transform.position);
 
+        DiceFace downwardFace = getFaceFacingDirection(Vector3.down);
+
+        // transfer tile's pips to player's face
         Transform[] pips = tile.getPips();
         foreach (Transform pip in pips) {
-            pip.parent = transform;
-        }   
+            pip.parent = downwardFace.transform;
+        }
+
+        downwardFace.value = tile.value;
+    }
+
+    void initializeFaces() {
+        foreach (DiceFace face in faces) {
+            GameObject go = new GameObject();
+            go.transform.parent = transform;
+            go.transform.localPosition = face.direction * 0.5f;
+            go.name = "Face " + face.name;
+            face.transform = go.transform;
+            face.value = 0;
+        }
+    }
+
+    DiceFace getFaceFacingDirection(Vector3 direction) {
+        Vector3 localDirection = transform.InverseTransformDirection(direction);
+
+        foreach (DiceFace face in faces) {
+            if (face.direction == localDirection) {
+                return face;
+            }
+        }
+        return faces[0];
+    }
+
+    bool canRollTo(Vector3 direction) {
+        Tile nextTile = grid.getTile(transform.position + direction);
+        if (nextTile == null) return false;
+
+        DiceFace nextFaceDown = getFaceFacingDirection(direction);
+
+        return nextFaceDown.value == 0;
     }
 
 }
